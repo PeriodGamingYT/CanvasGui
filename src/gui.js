b829a16380830e94e04c56e3f2f411011ce7b46b
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2020-05-05 18:26:58 -0600

    Update gui.js

--- src/gui.js
+++ src/gui.js
@@ -3,10 +3,31 @@ ctx.width = window.innerWidth;
 ctx.height = window.innerHeight;
 var canvas = ctx.getContext("2d");
 
+// Used Shapes.
+function roundRect(x, y, w, h, rad, s, f) {
+  var origStroke = canvas.strokeStyle;
+  var origFill = canvas.fillStyle;
+  canvas.strokeStyle = s;
+  canvas.fillStyle = f;
+  if(w < 2 * rad) rad = w / 2;
+  if(h < 2 * rad) rad = h / 2;
+  canvas.beginPath();
+  canvas.moveTo(x+rad, y);
+  canvas.arcTo(x+w, y, x+w,   y+h, rad);
+  canvas.arcTo(x+w, y+h, x,   y+h, rad);
+  canvas.arcTo(x,   y+h, x,   y,   rad);
+  canvas.arcTo(x,   y,   x+w, y,   rad);
+  canvas.stroke();
+  canvas.fill();
+  canvas.closePath();
+  canvas.strokeStyle = origStroke;
+  canvas.fillStyle = origFill;
+}
+
 // Window.
 var windows = [];
 
-function window(x, y, type, attributes) {
+function newWindow(x, y, type, attributes) {
   windows = [...windows,
     {
       x : x,
